b4acbfc57569654f6fe37d546c211eaf51f1c3f2
Israr Ahmad <israr.ahmad870@gmail.com>
2024-04-06 23:34:57 +0500

    first commit

---
+++ Floyd_Triangle/Floyd_triangle.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+async function Floyd(num) {
+    for (var i = 1; i <= num; i++) {
+        for (var j = 1; j < i + 1; j++) {
+            let p = i * (i - 1) / 2 + j;
+            process.stdout.write(chalk.greenBright(p + ' '));
+        }
+        console.log("\n");
+    }
+}
+let answer = await inquirer.prompt([
+    {
+        message: "Enter any number to areate Floyd's Triangle",
+        name: "Number",
+        type: "input"
+    },
+]);
+const num = parseInt(answer.Number);
+if (isNaN(num) || num < 1) {
+    console.log("Wrong input: Try Again");
+}
+else {
+    console.log(chalk.yellowBright("\t GENERATING FLOYD'S TRIANGLE"));
+    await Floyd(num);
+}
---
+++ Pascal_triangle/pascal.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+async function Pascals_triangle(num) {
+    let p = 1;
+    console.log(chalk.greenBright(p, "\n"));
+    for (var i = 1; i <= num; i++) {
+        process.stdout.write(chalk.greenBright(p + " "));
+        for (var j = 0; j < i; j++) {
+            p = p * (i - j) / (j + 1);
+            process.stdout.write(chalk.greenBright(p + ' '));
+        }
+        console.log("\n");
+    }
+}
+let answer = await inquirer.prompt([
+    {
+        message: "Enter any number to areate Floyd's Triangle",
+        name: "Number",
+        type: "input"
+    },
+]);
+const num = parseInt(answer.Number);
+if (isNaN(num) || num < 1) {
+    console.log("Wrong input: Try Again");
+}
+else {
+    console.log(chalk.yellowBright("\t GENERATING PASCAL'S TRIANGLE"));
+    await Pascals_triangle(num);
+}
